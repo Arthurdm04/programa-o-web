1d3d5a02045b2e16232215e8761e317be12a5432
Arthurdm04 <morescoarthur@gmail.com>
2025-03-28 09:47:44 -0300

    atividade

--- web/atividade3/app_calculadora/index.js
+++ web/atividade3/app_calculadora/index.js
@@ -1,41 +1,59 @@
 const express = require('express')
-const calc = require('./util/calculadora')
-
+const calc = require ('./util/calculadora')
 const app = express()
 
-app.get('/', (req, res)=> {
-    res.send('Olá, digite na URL o calculo que deseja fazer e em seguida os valores de a e b: somar1/2')
+
+app.get('/', (req, res) =>{
+    let html = '<h1>app_calculadora</h1>'
+    html += '<h3>Rotas disponiveis</h3>'
+    html += '<p>/somar/:a/:b (<a href ="/somar/2/3">somar/2/3</a>)</p>'
+    html += '<p>/subtrair/:a/:b (<a href ="/subtrair/2/3">subtrair/2/3</a>)</p>'
+    html += '<p>/multiplicar/:a/:b (<a href ="/multiplicar/2/3">multilicar/2/3</a>)</p>'
+    html += '<p>/dividir/:a/:b (<a href ="/dividir/2/3">dividir/2/3</a>)</p>'
+    res.send(html)
 })
 
-app.get('/somar/:a/:b', (req, res)=> {
-    const a = parseInt(req.params.a)
-    const b = parseInt(req.params.b)
-    const soma = calc.somar(a, b)
-    res.send(soma.toString())
+app.get('/somar/:a/:b', (req,res)=>{
+    let a = Number(req.params.a)
+    let b = Number(req.params.b)
+    let resultado = calc.somar(a,b)
+    if(isNaN(resultado)){
+        res.send('Não foi possível calcular')
+    }
+    res.send(`${a} + ${b} = ${calc.somar(a,b)}`)
 })
 
-app.get('/subtrair/:a/:b', (req, res) => {
-    const a = parseInt(req.params.a)
-    const b = parseInt(req.params.b)
-    const subtrair = calc.subtrair(a, b)
-    res.send(subtrair.toString())
+app.get('/subtrair/:a/:b', (req,res)=>{
+    let a = Number(req.params.a)
+    let b = Number(req.params.b)
+    let resultado = calc.subtrair(a,b)
+    if(isNaN(resultado)){
+        res.send('Não foi possível calcular')
+    }
+    res.send(`${a} - ${b} = ${calc.subtrair(a,b)}`)
 })
 
-app.get('/multiplicar/:a/:b', (req, res) => {
-    const a = parseInt(req.params.a)
-    const b = parseInt(req.params.b)
-    const multiplicacao = calc.multiplicar(a, b)
-    res.send(multiplicacao.toString())
+app.get('/multiplicar/:a/:b', (req,res)=>{
+    let a = Number(req.params.a)
+    let b = Number(req.params.b)
+    let resultado = calc.multiplicar(a,b)
+    if(isNaN(resultado)){
+        res.send('Não foi possível calcular')
+    }
+    res.send(`${a} * ${b} = ${calc.multiplicar(a,b)}`)
 })
 
-app.get('/dividir/:a/:b', (req, res) => {
-    const a = parseInt(req.params.a)
-    const b = parseInt(req.params.b)
-    const divisao = calc.dividir(a, b)
-    res.send(divisao.toString())
+app.get('/dividir/:a/:b', (req,res)=>{
+    let a = Number(req.params.a)
+    let b = Number(req.params.b)
+    let resultado = calc.dividir(a,b)
+    if(isNaN(resultado)){
+        res.send('Não foi possível calcular')
+    }
+    res.send(`${a} / ${b} = ${calc.dividir(a,b)}`)
 })
 
 const PORT = 8080
 app.listen(PORT, ()=>{
-    console.log('app rodando na port ' + PORT)
+    console.log('app rodando na porta' + PORT)
 })
